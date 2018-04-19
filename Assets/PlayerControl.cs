2df7ba669a18f16f942acb47d2741533c76dc215
MarkusZhang <zhangjie.iet@gmail.com>
2018-04-19 13:51:41 +0800

    first commit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	private float speed = 7;
+	private float screenHalfWidth;
+
+	// Use this for initialization
+	void Start () {
+
+		screenHalfWidth = Camera.main.aspect * Camera.main.orthographicSize;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		float velocity = inputX * speed;
+		transform.Translate (Vector2.right * velocity * Time.deltaTime);
+
+		// wrap around the scene
+		if (transform.position.x < -screenHalfWidth) {
+			transform.position = new Vector2(screenHalfWidth,transform.position.y);
+		} else if (transform.position.x > screenHalfWidth) {
+			transform.position = new Vector2(- screenHalfWidth, transform.position.y);
+		}
+
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		FindObjectOfType<GameOver> ().OnGameOver ();
+		Destroy (gameObject);
+		print ("player died");
+	}
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject blockPrefab;
+	private Vector2 screenHalfWidth;
+	private float secondsBetweenSpawns = 1;
+	private float nextSpawnTime;
+
+	// Use this for initialization
+	void Start () {
+		
+		screenHalfWidth = new Vector2 (Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+		//blockPrefab = GameObject.FindGameObjectWithTag ("FallingBlockPrefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawnTime) {
+			secondsBetweenSpawns = (float)Random.Range (0.2f, 0.5f);
+			nextSpawnTime = Time.time + secondsBetweenSpawns;
+			// spawn a new falling block
+			Vector2 position = new Vector2(Random.Range(-screenHalfWidth.x,screenHalfWidth.x),screenHalfWidth.y);
+			Instantiate (blockPrefab, position, Quaternion.identity);
+		}
+	}
+}
